91dc6b8d3be30567c5bfc67bddaaf8fc18b07b48
Tamás Szabó <r.tamas.szabo@gmail.com>
2022-03-16 11:17:49 +0100

    Scaffold Identity

---
+++ MandoWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MandoWebApp.Data;
+using MandoWebApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MandoWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace MandoWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
