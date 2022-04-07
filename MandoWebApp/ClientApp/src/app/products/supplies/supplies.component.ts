13b09ec18533b176bb18c6c787afc10084293e5a
Tamás Szabó <r.tamas.szabo@gmail.com>
2022-04-07 16:34:11 +0200

    Add supplies table

--- MandoWebApp/ClientApp/src/app/products/supplies/supplies.component.ts
+++ MandoWebApp/ClientApp/src/app/products/supplies/supplies.component.ts
@@ -1,20 +1,20 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Product } from '../../models/product';
+import { Supply } from 'src/app/models/supply';
 
 @Component({
   selector: 'app-supplies',
   templateUrl: './supplies.component.html'
 })
 export class SuppliesComponent {
-  public supplies: Product[] = [];
+  public supplies: Supply[] = [];
 
   constructor(private http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
     this.loadProducts();
   }
 
   private loadProducts(): void {
-    this.http.get<Product[]>(this.baseUrl + 'product/supplies').subscribe(result => {
+    this.http.get<Supply[]>(this.baseUrl + 'product/supplies').subscribe(result => {
       this.supplies = result;
     }, error => console.error(error));
   }
