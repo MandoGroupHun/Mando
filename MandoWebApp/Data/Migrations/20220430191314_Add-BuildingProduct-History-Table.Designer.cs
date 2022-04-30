ba35e271f5241b57ac01d3ab4410726c63677f6b
Gyorgy Valoczi <gyorgy.valoczi@logmein.com>
2022-04-30 21:15:34 +0200

    Add building product history table

--- MandoWebApp/Data/Migrations/20220430191314_Add-BuildingProduct-History-Table.Designer.cs
+++ MandoWebApp/Data/Migrations/20220430191314_Add-BuildingProduct-History-Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MandoWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MandoWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220430191314_Add-BuildingProduct-History-Table")]
+    partial class AddBuildingProductHistoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
