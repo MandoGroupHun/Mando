ac633295c112e961e78cae9360024aee1b2ca355
Mark Horvath <mark.horvath@asynth.com>
2022-05-03 20:35:03 +0200

    chore(build): add link for missing Identity assets

--- MandoWebApp/entrypoint.sh
+++ MandoWebApp/entrypoint.sh
@@ -8,5 +8,8 @@ export PATH="$PATH:/root/.dotnet/tools"
 # Run DB migrations
 dotnet ef database update
 
+# Create symlink for webroot content
+rm -rf ./wwwroot && ln -s ./out/wwwroot wwwroot
+
 # Start app
 dotnet "./bin/Release/net6.0/MandoWebApp.dll"
